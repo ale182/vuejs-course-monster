d840314124c2dcb8876728e00b8225124fde3141
Alexandre Souza <alexandre.souza@sharecare.com>
2020-08-05 01:00:31 -0300

    65. Adicionando o Registro de Log #01

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        monsterLife: 100,
+        logs: [],
     },
     computed: {
         hasResult() {
@@ -17,18 +18,20 @@ new Vue({
             this.monsterLife = 100
         },
         attack(especial) {
-            this.hurt('monsterLife', 5, 10, especial)
-            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if (this.monsterLife > 0)
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             // esta usando o max para que o valor nao fique negativo
             // chega no maximo em 0
             this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}`, cls)
         },
-        healAndHurt(){
-            this.heal(10,15)
+        healAndHurt() {
+            this.heal(10, 15)
             this.hurt('playerLife', 7, 12, false)
         },
         heal(min, max) {
@@ -39,6 +42,10 @@ new Vue({
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registerLog(text, cls) {
+            // unshift é pra colocar no começo do array
+            this.logs.unshift({text, cls})
         }
     },
     watch: {
