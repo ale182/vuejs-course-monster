6aa9b4fa29351a422c3b0997bbd31149d419dd9f
Alexandre Souza <alexandre.souza@sharecare.com>
2020-08-05 00:20:08 -0300

    62. Iniciar Jogo & Desistir do Jogo

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ new Vue({
     el: '#app',
     data: {
         running:false,
-        playerLife: 0,
-        monsterLife: 10
+        playerLife: 100,
+        monsterLife: 100
     },
     computed: {
         hasResult(){
             return this.playerLife == 0 || this.monsterLife == 0
         }
     },
-    methods: {},
+    methods: {
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        }
+    },
     watch: {}
 })
